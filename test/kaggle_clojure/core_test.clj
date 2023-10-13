1ec47558678197b4dfb932b93bc1a315df774030
neshamon <jmmatthews@proton.me>
2023-10-13 11:03:24 -0500

    Set up clojure project

---
+++ test/kaggle_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns kaggle-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [kaggle-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
