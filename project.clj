715b555599d813e864df8e78ff965e2506e31af6
neshamon <jmmatthews@proton.me>
2023-10-13 13:45:52 -0500

    Find average lot size

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [techascent/tech.ml.dataset "7.012"]]
-  :repl-options {:init-ns kaggle-clojure.core})
+  :repl-options {:init-ns kaggle-clojure.core}
+  :main kaggle-clojure.core)
--- src/kaggle_clojure/core.clj
+++ src/kaggle_clojure/core.clj
@@ -1,6 +1,29 @@
-(ns kaggle-clojure.core)
+(ns kaggle-clojure.core
+  (:require [tech.v3.dataset :as ds]))
 
-(defn foo
+(def house-path "resources/melb_data.csv")
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  []
+  (println "Working!"))
+
+(def melb-dataset
+  (ds/->dataset house-path))
+
+(def iowa-homes
+  (ds/->dataset "resources/train.csv"))
+
+(defn show-melb []
+  (println melb-dataset))
+
+(defn show-iowa []
+  (println iowa-homes))
+
+(def lot-area
+  (ds/select-columns iowa-homes ["LotArea"]))
+
+(defn averager [a]
+  (int (/ (reduce + (flatten (ds/rowvecs a))) (ds/row-count a))))
+
+(averager lot-area)
