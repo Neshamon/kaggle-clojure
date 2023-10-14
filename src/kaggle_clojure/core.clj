09d50aa133375f387f019df58b81c0209da7254c
neshamon <jmmatthews@proton.me>
2023-10-13 19:46:55 -0500

    Import iowa house dataset & complete exercises

--- src/kaggle_clojure/core.clj
+++ src/kaggle_clojure/core.clj
@@ -1,5 +1,6 @@
 (ns kaggle-clojure.core
-  (:require [tech.v3.dataset :as ds]))
+  (:require [tech.v3.dataset :as ds]
+            [tech.v3.dataset.io.datetime :as dt]))
 
 (def house-path "resources/melb_data.csv")
 
@@ -17,13 +18,30 @@
 (defn show-melb []
   (println melb-dataset))
 
+;; Iowa data set exercises begin here
+
+; What is the average lot size (rounded to nearest integer)?
+
 (defn show-iowa []
   (println iowa-homes))
 
 (def lot-area
   (ds/select-columns iowa-homes ["LotArea"]))
 
-(defn averager [a]
-  (int (/ (reduce + (flatten (ds/rowvecs a))) (ds/row-count a))))
+(defn averager [column]
+  (int (/ (reduce + (flatten (ds/rowvecs column))) (ds/row-count column))))
 
-(averager lot-area)
+(averager lot-area) ;;-> 10516. Off by 1?
+
+;; As of today, how old is the newest home? (current year - the date in which it was built)
+
+(def year-built
+  (ds/select-columns iowa-homes ["YearBuilt"]))
+
+(def max-year
+  (ds/sort-by-column year-built "YearBuilt" >))
+
+(defn measure-age [column]
+  (- 2023 (first (first (ds/rowvecs column)))))
+
+(measure-age max-year) ;; => 13
